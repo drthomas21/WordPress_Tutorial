824ea6c21cde6acfd332b39a74ab436f2b0b612f
David Thomas <david.thomas2331@gmail.com>
2017-09-24 18:58:57 -0700

    adding support for featured image

--- wp-content/plugins/seo-plugin/templates/metadata-default.php
+++ wp-content/plugins/seo-plugin/templates/metadata-default.php
@@ -1,21 +1,21 @@
 <!-- Page Data -->
 <meta http-equiv="Content-Type" content="text/html; charset=<?= get_bloginfo( 'charset' ); ?>">
 <?php if(is_category()):?>
-<meta property="og:title" content="Category |<?= single_cat_title(); ?>">
-<meta property="og:description" content="<?= get_bloginfo('description'); ?>">
-<meta property="description" content="<?= get_bloginfo('description'); ?>">
+    <meta property="og:title" content="Category |<?= single_cat_title(); ?>">
+    <meta property="og:description" content="<?= get_bloginfo('description'); ?>">
+    <meta property="description" content="<?= get_bloginfo('description'); ?>">
 <?php elseif(is_tag()):?>
-<meta property="og:title" content="Tag | <?= single_tag_title(); ?>">
-<meta property="og:description" content="<?= get_bloginfo('description'); ?>">
-<meta property="description" content="<?= get_bloginfo('description'); ?>">
+    <meta property="og:title" content="Tag | <?= single_tag_title(); ?>">
+    <meta property="og:description" content="<?= get_bloginfo('description'); ?>">
+    <meta property="description" content="<?= get_bloginfo('description'); ?>">
 <?php elseif(is_search()):?>
-<meta property="og:title" content="Search | <?= get_search_query(); ?>">
-<meta property="og:description" content="<?= get_bloginfo('description'); ?>">
-<meta property="description" content="<?= get_bloginfo('description'); ?>">
+    <meta property="og:title" content="Search | <?= get_search_query(); ?>">
+    <meta property="og:description" content="<?= get_bloginfo('description'); ?>">
+    <meta property="description" content="<?= get_bloginfo('description'); ?>">
 <?php else: ?>
-<meta property="og:title" content="<?= get_bloginfo('name'); ?>">
-<meta property="og:description" content="<?= get_bloginfo('description'); ?>">
-<meta property="description" content="<?= get_bloginfo('description'); ?>">
+    <meta property="og:title" content="<?= get_bloginfo('name'); ?>">
+    <meta property="og:description" content="<?= get_bloginfo('description'); ?>">
+    <meta property="description" content="<?= get_bloginfo('description'); ?>">
 <?php endif; ?>
 <meta property="og:url" content="<?= site_url($_SERVER['REQUEST_URI']); ?>">
 <link rel="canonical" href="<?= site_url($_SERVER['REQUEST_URI']); ?>">
--- wp-content/plugins/seo-plugin/templates/metadata-post.php
+++ wp-content/plugins/seo-plugin/templates/metadata-post.php
@@ -9,14 +9,20 @@
 <link rel="canonical" href="<?= get_permalink(); ?>">
 
 <!-- Image Data -->
-<?php foreach(get_children([
-    "post_parent" => $post->ID,
-    "post_mime_type" => "image",
-    "orderby" => "menu_order",
-    "order" => "ASC",
-    "numberposts" => 1
-]) as $idx => $Attachment): $data = wp_get_attachment_image_src($Attachment->ID,('original')); ?>
-<meta property="og:image" content="<?= $data[0]; ?>">
-<meta property="og:image:width" content="<?= $data[1]; ?>">
-<meta property="og:image:height" content="<?= $data[2]; ?>">
-<?php endforeach; ?>
+<?php if(get_post_thumbnail_id() > 0): $data = wp_get_attachment_image_src(get_post_thumbnail_id(),('original')); ?>
+    <meta property="og:image" content="<?= $data[0]; ?>">
+    <meta property="og:image:width" content="<?= $data[1]; ?>">
+    <meta property="og:image:height" content="<?= $data[2]; ?>">
+<?php else: ?>
+    <?php foreach(get_children([
+        "post_parent" => $post->ID,
+        "post_mime_type" => "image",
+        "orderby" => "menu_order",
+        "order" => "ASC",
+        "numberposts" => 1
+    ]) as $idx => $Attachment): $data = wp_get_attachment_image_src($Attachment->ID,('original')); ?>
+    <meta property="og:image" content="<?= $data[0]; ?>">
+    <meta property="og:image:width" content="<?= $data[1]; ?>">
+    <meta property="og:image:height" content="<?= $data[2]; ?>">
+    <?php endforeach; ?>
+<?php endif; ?>
