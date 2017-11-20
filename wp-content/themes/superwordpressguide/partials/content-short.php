193bee80efda302329fb34d515f291feba21ce47
David Thomas <david.thomas2331@gmail.com>
2017-11-19 16:59:16 -0800

    styling improvement

--- wp-content/themes/superwordpressguide/partials/content-short.php
+++ wp-content/themes/superwordpressguide/partials/content-short.php
@@ -1,12 +1,14 @@
 <section class="post clearfix">
     <div class="post-header">
         <h2 class="col-sm-12">
-            <span class="col-sm-12 col-md-12 col-lg-8"><a href="<?php the_permalink(); ?>" alt="<?= htmlentities(get_the_title(),ENT_QUOTES|ENT_HTML5); ?>"><?php the_title(); ?></a></span>
-            <span class="col-sm-12 col-md-12 col-lg-4 float-right"><small class="post-date">Posted on <?php the_date(); ?></small></span>
+            <a href="<?php the_permalink(); ?>" alt="<?= htmlentities(get_the_title(),ENT_QUOTES|ENT_HTML5); ?>"><?php the_title(); ?></a>
         </h2>
     </div>
-    <p>Categories: <?= get_the_category_link(); ?></p>
-    <div class="post-excerpt text-center">
+
+    <p class="col-xs-6 float-left">Categories: <?= get_the_category_link(); ?></p>
+    <p class="col-xs-6 float-right"><small class="post-date">Posted on <?php the_date(); ?></small></p>
+    <div class="clearfix"></div>
+    <div class="post-excerpt text-center col-xs-12">
         <?php the_featured_content(); ?>
     </div>
     <span class="read-more"><a href="<?php the_permalink(); ?>" alt="<?= htmlentities(get_the_title(),ENT_QUOTES|ENT_HTML5); ?>">Read More &gt;</a></span>
