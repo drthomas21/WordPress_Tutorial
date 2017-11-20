708896cc517bf2b4c7f0f84d2451fda2842c6d91
David Thomas <david.thomas2331@gmail.com>
2017-11-19 16:56:47 -0800

    implementing youtube plugin on theme

--- wp-content/themes/superwordpressguide/partials/sidebar-youtube.php
+++ wp-content/themes/superwordpressguide/partials/sidebar-youtube.php
@@ -1,12 +1,8 @@
 <div class="youtube-vids">
     <h2 class='text-center'>Hot Youtube Vids</h2>
-    <div class="embed-responsive embed-responsive-16by9">
-        <iframe class='embed-responsive-item' src="https://www.youtube.com/embed/Q752i7MKLwU" frameborder="0" allowfullscreen></iframe>
-    </div>
-    <div class="embed-responsive embed-responsive-16by9">
-        <iframe class='embed-responsive-item' src="https://www.youtube.com/embed/hIDj5jQ4SxE" frameborder="0" allowfullscreen></iframe>
-    </div>
-    <div class="embed-responsive embed-responsive-16by9">
-        <iframe class='embed-responsive-item' src="https://www.youtube.com/embed/uXT6iQ31MKc" frameborder="0" allowfullscreen></iframe>
-    </div>
+    <?php foreach(list_popular_videos(0,5) as $Video): ?>
+        <div class="embed-responsive embed-responsive-16by9">
+            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/<?= $Video->id; ?>?rel=0" allowfullscreen></iframe>
+        </div>
+    <?php endforeach; ?>
 </div>
