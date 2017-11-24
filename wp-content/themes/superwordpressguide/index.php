174c7a4eafe6af00526a3fa1397207049cc144e6
David Thomas <david.thomas2331@gmail.com>
2017-11-24 00:49:57 -0800

    adding adSense (testing it out)

--- wp-content/themes/superwordpressguide/index.php
+++ wp-content/themes/superwordpressguide/index.php
@@ -47,6 +47,17 @@
                 <?php endforeach; ?>
             </div>
         <?php endif; ?>
+        <div class="goole-ad">
+            <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+            <!-- homepage -->
+            <ins class="adsbygoogle"
+                 style="display:inline-block;width:300px;height:600px"
+                 data-ad-client="ca-pub-6823528022937171"
+                 data-ad-slot="7968861962"></ins>
+            <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
+        </div>
     </div>
 </section>
 
--- wp-content/themes/superwordpressguide/single.php
+++ wp-content/themes/superwordpressguide/single.php
@@ -1,11 +1,34 @@
 <?php get_header(); ?>
 <section class="single blog-header">
     <div class="page-container">
-        <div class="col-lg-12 col-xl-8 posts">
+        <div class="col-sm-8 col-xl-8 posts float-left">
             <?php if(have_posts()): while(have_posts()): the_post(); ?>
                 <?php get_template_part("partials/content","single"); ?>
             <?php endwhile; else: ?>
                 <?php get_template_part("partials/content","empty"); ?>
+            <?php endif; ?>
+        </div>
+        <div class="col-sm-4 col-xl-4 float-right text-center">
+            <div class="goole-ad">
+                <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                <!-- article_page -->
+                <ins class="adsbygoogle"
+                     style="display:inline-block;width:300px;height:600px"
+                     data-ad-client="ca-pub-6823528022937171"
+                     data-ad-slot="5361989274"></ins>
+                <script>
+                (adsbygoogle = window.adsbygoogle || []).push({});
+                </script>
+            </div>
+            <?php if(!empty(list_popular_videos(0,2))): ?>
+                <div class="youtube-vids">
+                    <h2 class='text-center section-label'>Going Viral?</h2>
+                    <?php foreach(list_popular_videos(0,3) as $Video): ?>
+                        <div class="embed-responsive embed-responsive-16by9">
+                            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/<?= $Video->id; ?>?rel=0" allowfullscreen></iframe>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
             <?php endif; ?>
         </div>
     </div>
