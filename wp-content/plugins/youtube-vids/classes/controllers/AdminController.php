3223593a1901cc67f7416041b909a7dadb85ce4c
David Thomas <david.thomas2331@gmail.com>
2017-11-23 22:54:37 -0800

    adding support for refreshing access token

--- wp-content/plugins/youtube-vids/classes/controllers/AdminController.php
+++ wp-content/plugins/youtube-vids/classes/controllers/AdminController.php
@@ -21,10 +21,30 @@ class AdminController {
         add_action( 'admin_menu', function() {
             $this->adminMenu();
         });
+
+        add_action("admin_init",function() {
+            if($_GET['action'] == self::ACTION_AUTH && isset($_GET['code'])) {
+                $Driver = $this->getDriver();
+                $Driver->authenticate($_GET['code']);
+                wp_safe_redirect($this->getPageURL());
+                exit();
+            }
+        });
+    }
+
+    protected function getDriver(): \Youtube_Vids\Drivers\GoogleApiDriver {
+        $Driver = new \Youtube_Vids\Drivers\GoogleApiDriver();
+        $Driver->setRedirectUri($this->getRedirectURL());
+        $Driver->prepareScopes();
+        return $Driver;
     }
 
     protected function getRedirectURL(): string {
         return admin_url("options-general.php?page=".self::MENU_SLUG."&action=".self::ACTION_AUTH);
+    }
+
+    protected function getPageURL(): string {
+        return admin_url("options-general.php?page=".self::MENU_SLUG);
     }
 
     protected function adminMenu() {
@@ -34,13 +54,13 @@ class AdminController {
     }
 
     protected function display() {
-        $Driver = new \Youtube_Vids\Drivers\GoogleApiDriver();
-        $Driver->setRedirectUri($this->getRedirectURL());
-        $Driver->prepareScopes();
-
-        if($_GET['action'] == self::ACTION_AUTH) {
-            $Driver->authenticate($_GET['code']);
-        }
+        $Driver = $this->getDriver();
+        $Driver->setAccessToken();
+        $token = $Driver->getAccessToken();
+        
+        $accessToken = isset($token['access_token']) ? $token['access_token'] : "";
+        $createDate = isset($token['created']) ? date("Y-m-d H:i:s",$token['created'] - (8 * 3600)) : "";
+        $expireDate = isset($token['created']) && $token['expires_in'] ? date("Y-m-d H:i:s",$token['created'] + $token['expires_in'] - (8 * 3600)) : "";
 
         include YOUTUBE_VIDS_DIR.'/templates/index.php';
     }
--- wp-content/plugins/youtube-vids/classes/drivers/GoogleApiDriver.php
+++ wp-content/plugins/youtube-vids/classes/drivers/GoogleApiDriver.php
@@ -9,6 +9,7 @@ class GoogleApiDriver {
         $this->AccountRecord = \Youtube_Vids\Models\AccountModel::getAccountRecord();
         $this->GoogleClient = new \Google_Client();
         $this->GoogleClient->setApplicationName("Youtube Vids");
+        $this->GoogleClient->setAccessType("offline");
         $this->GoogleClient->setClientSecret($this->AccountRecord->secret);
         $this->GoogleClient->setClientId($this->AccountRecord->id);
         $this->GoogleClient->setScopes('https://www.googleapis.com/auth/youtube');
@@ -20,21 +21,33 @@ class GoogleApiDriver {
     }
 
     public function checkAccessToken(): bool {
-        $token = get_option(self::OPTION_ACCESS_TOKEN,"");
-        if(!empty($token)) {
-            $this->GoogleClient->setAccessToken($token);
-            return (bool)$this->GoogleClient->getAccessToken();
+        $token = $this->getAccessToken();
+        if(is_array($token) && array_key_exists("access_token",$token) && array_key_exists("expires_in",$token) && array_key_exists("created",$token)) {
+            return time() < (int)($token['created'] + $token['expires_in']);
         }
 
         return false;
     }
 
-    public function createAuthUrl(): string {
-        return $this->GoogleClient->createAuthUrl();
+    public function getAccessToken(): array {
+        $token = get_option(self::OPTION_ACCESS_TOKEN,[]);
+        return !is_array($token) ? [] : $token;
     }
 
-    public function getAccessToken(): string {
-        return get_option(self::OPTION_ACCESS_TOKEN,"");
+    public function setAccessToken(): bool {
+        if($this->checkAccessToken()) {
+            $this->GoogleClient->setAccessToken($this->getAccessToken());
+            return true;
+        } else {
+            $token = $this->GoogleClient->getAccessToken();
+            update_option(self::OPTION_ACCESS_TOKEN,$token);
+            return true;
+        }
+        return false;
+    }
+
+    public function createAuthUrl(): string {
+        return $this->GoogleClient->createAuthUrl();
     }
 
     public function getClient(): \Google_Client {
