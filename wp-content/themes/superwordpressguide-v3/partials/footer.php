875a5b8356adaeace47ab35c32ae84e9176c47c2
David Thomas <david.thomas2331@gmail.com>
2018-05-20 11:21:09 -0700

    finalizing changes

--- wp-content/themes/superwordpressguide-v3/partials/footer.php
+++ wp-content/themes/superwordpressguide-v3/partials/footer.php
@@ -1,10 +1,17 @@
 <footer class='text-center'>
     <div ng-controller="ConsentCtrl" class="consent-banner hide" ng-hide="consentGiven">
-        This site uses Google Analytics to record pageviews. If you are not okay
-        with that, then please stop vising this site.
-        See our <a href="<?= get_privacy_policy_url(); ?>" target="_self">Privacy Policy</a>
-        for more details.
-        <button class="btn btn-outline-light" type="submit" ng-click="consent()">I Agree</button>
+        <div class="row">
+            <div class="col-sm-12">
+                This site uses <a href="https://tools.google.com/dlpage/gaoptout" target="_blank">Google Analytics</a> to record pageviews. Outside of that,
+                we do not collect any information. By staying on this site, you are giving us your consent.
+                <button class="btn btn-outline-light" type="submit" ng-click="consent()">I Agree</button>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-12">
+                See our <a href="<?= get_privacy_policy_url(); ?>" target="_self">Privacy Policy</a> for more details.
+            </div>
+        </div>
     </div>
     <ul>
         <li><a href="<?= site_url("terms-of-service"); ?>">Terms of Service</a></li>
