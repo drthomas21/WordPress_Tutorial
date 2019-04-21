1a20f2599601f9a11cb7bc99d175709eac1e809a
drthomas21 <david.thomas2331@gmail.com>
2019-04-20 21:00:32 -0700

    adding forum and moving stuff around

---
+++ community_htdocs/config.php
@@ -0,0 +1,22 @@
+<?php return array (
+  'debug' => false,
+  'database' => 
+  array (
+    'driver' => 'mysql',
+    'host' => 'mysql-wordpress',
+    'database' => 'community_guide',
+    'username' => 'wpcommunity_guide',
+    'password' => 't%y@Skw!P#!A6vkE',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => 'community_',
+    'port' => '3306',
+    'strict' => false,
+  ),
+  'url' => 'https://dev.community.superwordpressguide.com',
+  'paths' => 
+  array (
+    'api' => 'api',
+    'admin' => 'admin',
+  ),
+);
